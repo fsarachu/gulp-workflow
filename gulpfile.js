62a61049325b952a842af9631f3eb5811f93c58a
Franco Sarachu <fsarachu@gmail.com>
2016-07-29 04:30:07 -0300

    Add globbing to gulp.src()

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function () {
-  return gulp.src('src/scss/styles.scss')
+  return gulp.src('src/scss/**/*.scss')
       .pipe(sass())
       .pipe(gulp.dest('src/css'))
 });
