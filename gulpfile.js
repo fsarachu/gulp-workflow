ffd0e08cffbb5d60f92c43d10caa38571479ffee
Franco Sarachu <fsarachu@gmail.com>
2016-07-29 03:53:14 -0300

    Scaffold project

--- gulpfile.js
+++ gulpfile.js
@@ -3,3 +3,7 @@ var gulp = require('gulp');
 gulp.task('default', function() {
   // place code for your default task here
 });
+
+gulp.task('hello', function () {
+  console.log('Hey hey heyy!');
+});
