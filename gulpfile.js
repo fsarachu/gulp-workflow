12f0d5083367a1fbb06df85f100f2cfc17662cf0
Franco Sarachu <fsarachu@gmail.com>
2016-07-31 02:42:16 -0300

    Add minify html task and update watch

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var cleanCss = require('gulp-clean-css');
+var htmlmin = require('gulp-htmlmin');
 var browserSync = require('browser-sync').create();
 
 gulp.task('sass', function () {
@@ -22,19 +23,30 @@ gulp.task('concat-css', ['sass'], function () {
 gulp.task('minify-css', ['concat-css'], function () {
   return gulp.src('src/css/all.css')
       .pipe(cleanCss())
-      .pipe(gulp.dest('dist'));
+      .pipe(gulp.dest('dist/css'))
+      .pipe(browserSync.reload({
+        stream: true
+      }))
 });
 
-gulp.task('watch', ['browser-sync', 'sass'], function () {
-  gulp.watch('src/scss/**/*.scss', ['sass']);
-  gulp.watch('src/*.html', browserSync.reload);
-  gulp.watch('src/**/*.js', browserSync.reload);
+gulp.task('minify-html', function () {
+  return gulp.src('src/*.html')
+      .pipe(htmlmin({collapseWhitespace: true}))
+      .pipe(gulp.dest('dist'))
+      .pipe(browserSync.reload({
+        stream: true
+      }))
+});
+
+gulp.task('watch', ['browser-sync', 'sass', 'minify-css', 'minify-html'], function () {
+  gulp.watch('src/scss/**/*.scss', ['sass', 'minify-css']);
+  gulp.watch('src/*.html', ['minify-html']);
 });
 
 gulp.task('browser-sync', function () {
   browserSync.init({
     server: {
-      baseDir: 'src'
+      baseDir: 'dist'
     }
   });
 });
